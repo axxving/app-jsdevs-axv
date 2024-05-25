7f4bb6365c60aaa5cae44a752ea92f714253e601
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-05-25 15:02:20 -0600

    feat(): styles of the component footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { Contacto } from "./components/Contacto"
-import { Header } from "./components/Header"
-import { Hero } from "./components/Hero"
-import { Servicios } from "./components/Servicios"
-import { Sobre } from "./components/Sobre"
-import { Tecnologias } from "./components/Tecnologias"
+import { Contacto } from "./components/Contacto";
+import { Header } from "./components/Header";
+import { Hero } from "./components/Hero";
+import { Servicios } from "./components/Servicios";
+import { Sobre } from "./components/Sobre";
+import { Tecnologias } from "./components/Tecnologias";
+import { Footer } from "./components/Footer";
 
 export const App = () => {
   return (
@@ -14,6 +15,7 @@ export const App = () => {
       <Sobre />
       <Tecnologias />
       <Contacto />
+      <Footer />
     </>
   )
 }
