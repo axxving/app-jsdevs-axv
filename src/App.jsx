336be681015fe7520d67f4face1839427fad33ce
axxving <axxving@gmail.com>
2024-12-19 17:26:38 -0600

    feat: cleaning up project

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,5 @@
-import { Contacto } from "./components/Contacto";
-import { Header } from "./components/Header";
-import { Hero } from "./components/Hero";
-import { Servicios } from "./components/Servicios";
-import { Sobre } from "./components/Sobre";
-import { Tecnologias } from "./components/Tecnologias";
-import { Footer } from "./components/Footer";
-import { ScrollToTopButton } from "./components/ScrollToTopButton";
+import React from 'react';
 
 export const App = () => {
-  return (
-    <>
-      <Header />
-      <Hero />
-      <Servicios />
-      <Sobre />
-      <Tecnologias />
-      <Contacto />
-      <Footer />
-      <ScrollToTopButton />
-    </>
-  )
-}
+    return <div>App</div>;
+};
